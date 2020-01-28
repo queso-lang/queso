6daf51980975a205fa8b5ca390883a1dd858dbb6
judehunter <matisowagm@gmail.com>
2020-01-28 20:18:06 +0100

    Tweak formatting for expressions

--- src/ast.rs
+++ src/ast.rs
@@ -2,20 +2,23 @@ use crate::*;
 
 #[derive(Debug)]
 pub enum Expr {
-    Number(Token),
+    Constant(Token),
     Grouping(Box<Expr>),
     Binary(Box<Expr>, Token, Box<Expr>),
-    Unary(Token, Box<Expr>)
+    Unary(Token, Box<Expr>),
+
+    Error
 }
 
 impl std::fmt::Display for Expr {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Expr::Number(tok) => write!(f, "{}", tok.val),
+            Expr::Constant(tok) => write!(f, "`{}`", tok.val),
             Expr::Grouping(expr) => write!(f, "{}", **expr),
             Expr::Binary(left, op, right) => 
                 write!(f, "({} {} {})", op.val, **left, **right),
-            Expr::Unary(tok, right) => write!(f, "{}{}", tok.val, **right)
+            Expr::Unary(tok, right) => write!(f, "{}{}", tok.val, **right),
+            _ => panic!("display trait not defined")
         }
     }
 }
