7a0b57ae97e7e3c5ef1e84f8850ab4acd7aeab26
judehunter <matisowagm@gmail.com>
2020-01-30 23:28:42 +0100

    Compiling constants

--- src/compiler.rs
+++ src/compiler.rs
@@ -21,7 +21,11 @@ impl Token {
     }
 }
 
-impl Expr {
+trait Compile {
+    fn compile(&self, chk: &mut Chunk);
+}
+
+impl Compile for Expr {
     fn compile(&self, chk: &mut Chunk) {
         match self {
             Expr::Constant(tok) => {
@@ -30,5 +34,44 @@ impl Expr {
             },
             _ => unimplemented!()
         }
+    }
+}
+
+impl Compile for Stmt {
+    fn compile(&self, chk: &mut Chunk) {
+        match self {
+            Stmt::Expr(expr) => expr.compile(chk),
+            _ => unimplemented!()
+        }
+    }
+}
+
+struct Compiler;
+impl Compiler {
+    pub fn compile<T: Compile>(&self, chk: &mut Chunk, ast: T) {
+        ast.compile(chk);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_constant() {
+        let mut chk = Chunk::new();
+        let token = Token {
+            pos: TokenPos {from_col: 0, to_col: 1, line: 1},
+            t: TokenType::Number,
+            val: "1.23".to_string()
+        };
+        let expr = Expr::Constant(token);
+        let expr_stmt = Stmt::Expr(expr);
+
+        let compiler = Compiler {};
+        compiler.compile(&mut chk, expr_stmt);
+
+        assert_eq!(chk.get_const(0).clone(), Value::Number(1.23));
+        assert_eq!(chk.get_line_no(0), 1);
     }
 }
