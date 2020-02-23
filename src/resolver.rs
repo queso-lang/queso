352df3ce7746ce594f9435841b6354a15a76e1e6
judehunter <matisowagm@gmail.com>
2020-02-23 13:18:13 +0100

    Resolving inside if..else expressions

--- src/resolver.rs
+++ src/resolver.rs
@@ -90,6 +90,15 @@ impl Resolver {
                 let right = self.resolve_expr(*right)?;
                 Ok(Expr::Unary(op, Box::new(right)))
             },
+            Expr::IfElse(cond, if_branch, else_branch) => {
+                let cond = self.resolve_expr(*cond)?;
+                let ifb = self.resolve_expr(*if_branch)?;
+                let mut eb = else_branch;
+                if let Some(else_branch_expr) = eb {
+                    eb = Some(Box::new(self.resolve_expr(*else_branch_expr)?));
+                }
+                Ok(Expr::IfElse(Box::new(cond), Box::new(ifb), eb))
+            },
             _ => Ok(expr)
         }
     }
